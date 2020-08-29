6d89df94ea1d443885ca8cecc20df402cd7d912e
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-08-29 22:21:01 +0400

    Add project files.

---
+++ NotificationSignalR/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/StudentHub").build();
+    connection.start();
+    
+    $(document).on("click", "#send", function () {
+        var message = $("#message").val();
+
+
+        connection.invoke("SendMessage", message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    })
+    var notfMes;
+    connection.on("ReceiveMessage", function (message) {
+
+        //var list = $("#messageDropdown");
+        //var li = $(`<li>${message}</li>`)
+        //$(li).appendTo(list);
+        notfMes = message;
+
+    })
+    $(document).on("click", "#badgeButton", function () {
+        $("#messageBox").notify(`${notfMes}`);
+    })
+
+
+})
+
